75f885ddc00286e5629c1dafa9769faafe4ee947
Anton <Antik125@ya.ru>
2016-07-21 03:25:12 +0300

    Compatibility info.

--- kul.py
+++ kul.py
@@ -1,5 +1,6 @@
 """
 Script updates article list in ruwiki's {{Случайные статьи с КУЛ}} template.
+Written in python 3.5, compatibility with older versions doesn't tested.
 
 Usage:
     python kul.py
