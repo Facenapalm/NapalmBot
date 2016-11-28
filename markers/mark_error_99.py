c55cabc1049b57aba2f4539526dcdbc9c54e1be6
Anton <Antik125@ya.ru>
2016-11-28 14:32:30 +0300

    Markers for 94, 98 and 99 errors

---
+++ markers/mark_error_99.py
@@ -0,0 +1,21 @@
+"""Marks all fixed errors #99 on ruwiki's CheckWikipedia."""
+import re
+import pywikibot
+from checkwiki import load_page_list, check_tag_balance, mark_error_done, log
+
+NUMBER = "99"
+FLAGS = re.I
+
+def main():
+    """Downloads list from server and marks relevant errors as done."""
+    site = pywikibot.Site()
+    for line in load_page_list(NUMBER):
+        page = pywikibot.Page(site, line)
+        if check_tag_balance(page.text, "sup"):
+            mark_error_done(NUMBER, page.title())
+            log(line, success=True)
+        else:
+            log(line, success=False)
+
+if __name__ == "__main__":
+    main()
