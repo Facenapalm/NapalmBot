2c932a14fbf6a468176af77b39360be108777389
Anton <Antik125@ya.ru>
2017-02-28 19:31:59 +0300

    Add marker for 104th error

---
+++ markers/mark_error_104.py
@@ -0,0 +1,29 @@
+"""Marks all fixed errors #104 on ruwiki's CheckWikipedia."""
+import re
+import pywikibot
+from checkwiki import load_page_list, mark_error_done, log
+
+NUMBER = "104"
+REGEXP = r"<ref\s+name=\s*(.*?)\s*(?:group=.*?)?\s*/?>"
+FLAGS = re.I
+
+def main():
+    """Downloads list from server and marks relevant errors as done."""
+    site = pywikibot.Site()
+    for line in load_page_list(NUMBER):
+        page = pywikibot.Page(site, line)
+        mark = True
+        for name in re.findall(REGEXP, page.text, flags=FLAGS):
+            if re.match(r"^'.*'$|^\".*\"$", name):
+                continue
+            if re.search(r"[\"'/\\=?#>\s]", name):
+                mark = False
+                break
+        if mark:
+            mark_error_done(NUMBER, page.title())
+            log(line, success=True)
+        else:
+            log(line, success=False)
+
+if __name__ == "__main__":
+    main()
