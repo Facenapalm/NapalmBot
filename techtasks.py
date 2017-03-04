c625580b6638d109af7f3ae00fd02acc885baeb7
Anton <Antik125@ya.ru>
2017-03-04 20:59:16 +0300

    Update techtasks.py

--- techtasks.py
+++ techtasks.py
@@ -15,25 +15,16 @@ from checkwiki import ignore, deignore
 CATEGORY_NAME = "Категория:Википедия:Обсуждения с нерешёнными техническими задачами"
 TEMPLATE_NAME = "техзадача"
 DONE_PARAM = "выполнено"
-
 SORT_PARAM = "дата"
 DEFAULT_SORTKEY = "0000-00-00"
-SORT_REVERSE = True
-
-RESULT_PAGE = "Проект:Технические работы/Задачи из обсуждений"
-RESULT_BEGINNING = "{{/шапка}}"
-RESULT_FORMAT = "{{{{/задача|{params}|ссылка={link}}}}}"
-RESULT_ENDING = "{{/подвал}}"
 
 COMMENT = "Автоматическое обновление списка техзадач."
 
+LOG = True
 LOG_FORMAT = "Processed {done} out of {count} pages ({percentage} %)."
 
-TEMPLATE_REGEXP = r"^\{\{\s*" + TEMPLATE_NAME + r"\s*\|"
-IGNORE_FILTER = r"<nowiki>(.*?)</nowiki>|<nowiki\s*/>"
-
 def find_heading(code, node):
-    """Finds first second-level heading before the node."""
+    """Find first second-level heading before the mwparserfromhell node."""
     index = code.index(node, recursive=True)
     for heading in list(code.nodes)[index::-1]:
         if not isinstance(heading, mwparserfromhell.nodes.heading.Heading):
@@ -43,8 +34,8 @@ def find_heading(code, node):
         return heading
     return None
 
-def encode_string(text):
-    """Replaces special symbols by its codes."""
+def encode_string_link(text):
+    """Replace special symbols with its codes."""
     text = text.replace("<", "%3C")
     text = text.replace(">", "%3E")
     text = text.replace("[", "%5B")
@@ -54,12 +45,32 @@ def encode_string(text):
     text = text.replace("|", "%7C")
     return text
 
-def linkify_heading(text):
+def encode_string_text(text):
+    """Replace special symbols with corresponding entities or magicwords."""
+    text = text.replace("<", "&lt;")
+    text = text.replace(">", "&gt;")
+    text = text.replace("[", "&#91;")
+    text = text.replace("]", "&#93;")
+    text = text.replace("{", "&#123;")
+    text = text.replace("}", "&#125;")
+    text = text.replace("|", "{{!}}")
+    return text
+
+def delete_markup(text, site=None, encoder=None):
     """
-    Converts heading text to link part, which goes after #.
-    For example: "== [[Something|head]]''ing'' ==" -> "heading".
+    Delete all wikimarkup (links, tags, etc) from text.
+    First parameter is text to process.
+    Second parameter is pywikibot.Site object used for text expansion.
+    Third parameter is encoder function - callback which processed text and
+    replaces special symbols (<, >, |, etc). It is neccessary for the text
+    which was inside the <nowiki> tag.
     """
-    (text, ignored) = ignore(text, IGNORE_FILTER)
+    ignore_filter = r"<nowiki>(.*?)</nowiki>|<nowiki\s*/>"
+
+    if site is not None:
+        text = site.expand_text(text)
+
+    (text, ignored) = ignore(text, ignore_filter)
     text = re.sub(r"<!--.*?-->", "", text)
     text = re.sub(r"\[\[:?(?:[^|]*\|)?([^\]]*)\]\]", "\\1", text)
     text = re.sub(r"\[https?://[^ \]]+ ([^\]]+)\]", "\\1", text)
@@ -68,23 +79,27 @@ def linkify_heading(text):
     text = re.sub(r"<[a-z]+(?:\s*[a-z]+\s*=[^<>]+)?\s*/?>|</[a-z]+>", "", text, flags=re.I)
     text = deignore(text, ignored)
 
-    text = re.sub(IGNORE_FILTER, "\\1", text)
-    text = encode_string(text)
+    text = re.sub(ignore_filter, "\\1", text)
+    if encoder is not None:
+        text = encoder(text)
 
     text = re.sub(r"[ ]{2,}", " ", text)
     text = text.strip()
     return text
 
 def log(done, count):
-    """Writes information about progress."""
+    """Write information about progress."""
+    if not LOG:
+        return
     if count == 0:
         percentage = 100
     else:
         percentage = round(100 * done / count)
     print(LOG_FORMAT.format(done=done, count=count, percentage=percentage))
 
-def main():
-    """Updates list of technical tasks."""
+def update_techtasks(pagename, beginning, line_format, ending, clear=False,
+                     reverse_sort=True):
+    """Update list of technical tasks."""
     site = pywikibot.Site()
     pages = list(pywikibot.Category(site, CATEGORY_NAME).members())
     count = len(pages)
@@ -100,37 +115,59 @@ def main():
         # And it can lead to the error. So, to prevent mwparserfromhell bug:
         text = re.sub(r"(?:'''''|'''|'')", "", text)
         code = mwparserfromhell.parse(text)
-        for template in code.filter_templates(matches=TEMPLATE_REGEXP):
+        for template in code.filter_templates(matches=r"^\{\{\s*" + TEMPLATE_NAME + r"\s*\|"):
             if template.has(DONE_PARAM, ignore_empty=True):
                 continue
-            params = "|".join([str(param) for param in template.params])
+            param_list = []
+            for param in template.params:
+                param = str(param)
+                if clear:
+                    param = delete_markup(param, site, encode_string_text)
+                param_list.append(param)
+            params = "|".join(param_list)
 
             heading = find_heading(code, template)
             if heading is None:
                 link = page.title()
             else:
                 heading = str(heading.title)
-                link = page.title() + "#" + linkify_heading(heading)
+                link = page.title() + "#" + delete_markup(heading, site, encode_string_link)
 
             if template.has(SORT_PARAM, ignore_empty=True):
                 sortkey = template.get(SORT_PARAM).value.strip()
             else:
                 sortkey = DEFAULT_SORTKEY
 
-            line = RESULT_FORMAT.format(params=params, link=link)
+            line = line_format.format(params=params, link=link)
             templates.append((line, sortkey))
 
     log(count, count)
-    templates.sort(key=lambda template: template[1], reverse=SORT_REVERSE)
+    templates.sort(key=lambda template: template[1], reverse=reverse_sort)
 
     lines = []
-    lines.append(RESULT_BEGINNING)
-    lines += [template[0] for template in templates]
-    lines.append(RESULT_ENDING)
+    if len(beginning) > 0:
+        lines.append(beginning)
+    for template in templates:
+        lines.append(template[0])
+    if len(ending) > 0:
+        lines.append(ending)
 
-    page = pywikibot.Page(site, RESULT_PAGE)
+    page = pywikibot.Page(site, pagename)
     page.text = "\n".join(lines)
     page.save(COMMENT, minor=False)
+
+def main():
+    """Updates lists in ruwiki."""
+    update_techtasks("Проект:Технические работы/Задачи из обсуждений",
+                     "{{/шапка}}",
+                     "{{{{/задача|{params}|ссылка={link}}}}}",
+                     "{{/подвал}}",
+                     clear=False)
+    update_techtasks("Проект:Технические работы/Задачи из обсуждений/Актуальные задачи/Список задач",
+                     "",
+                     "{{{{Проект:Технические работы/Задачи из обсуждений/Актуальные задачи/Список задач/задача|{params}|ссылка={link}}}}}",
+                     "",
+                     clear=True)
 
 if __name__ == "__main__":
     main()
