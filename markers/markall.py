d660467b3ea13db726d4361999fc120b2c5c2fbf
tools.napalmbot <tools.napalmbot@tools-bastion-03.tools.eqiad.wmflabs>
2017-03-14 00:56:03 +0000

    update markall.py

--- markers/markall.py
+++ markers/markall.py
@@ -1,10 +1,16 @@
 """Executes every mark_error_*.py script from current directory."""
 
+import sys
 import os
 import re
 
 if __name__ == "__main__":
-    for filename in os.listdir():
+    if len(sys.argv) == 1:
+        dir = "."
+    else:
+        dir = sys.argv[1]
+    for filename in os.listdir(dir):
         if re.match(r"^mark_error_\d+\.py", filename):
             print("{}:".format(filename))
-            exec(open(filename, encoding="utf-8").read())
+            exec(open(os.path.join(dir, filename), encoding="utf-8").read())
+
