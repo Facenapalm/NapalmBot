8b2bd0aab59010438f18dd9df93d9873041c6110
Anton Facenapalm <Anton Facenapalm>
2017-04-03 05:16:24 +0300

    fastzka.py request correction

--- fastzka.py
+++ fastzka.py
@@ -9,20 +9,41 @@ import re
 from datetime import datetime
 import pywikibot
 
-REGEXP = re.compile(r"\n*==.*?==\n<onlyinclude>((?:[^<]|<(?!onlyinclude))*?)</onlyinclude>",
-                    flags=re.I)
+REGEXP = re.compile(r"""
+    (?P<indent>\n*)
+    ==\s*(?P<header>.*?)\s*==\s+
+    (?P<section>
+        <onlyinclude>\s*
+        (?:[^<]|<(?!/?onlyinclude))*?\s*
+        </onlyinclude>
+    )
+""", re.I | re.VERBOSE)
 UTCNOW = datetime.utcnow()
 
 def correct_request(match):
     """
     Fix some errors, for example, update header if it doesn't match the content.
-    Doesn't implemented yet.
     """
-    return match.group(0)
+    indent = match.group("indent")
+    header = match.group("header")
+    template = match.group("section")
+
+    question = re.search(r"\|\s*вопрос\s*=(.*)", template)
+    timestamp = re.search(r"\|\s*автор\s*=[^/]+/\s*(\d{14})", template)
+    if question is None or timestamp is None:
+        # request is completely broken
+        return match.group(0)
+
+    correct_header = question.group(1).strip() + "/" + timestamp.group(1)
+    if header == correct_header:
+        # all is ok
+        return match.group(0)
+    
+    return "{}== {} ==\n{}".format(indent, correct_header, template)
 
 def delete_old_request(match):
     """Process one table row and delete it if it's neccessary."""
-    template = match.group(1)
+    template = match.group("section")
     status_match = re.search(r"\|\s*статус\s*=\s*([+-])", template)
     date_match = re.search(r"\|\s*администратор\s*=[^/]+/\s*(\d{14})", template)
     if status_match is None or date_match is None:
