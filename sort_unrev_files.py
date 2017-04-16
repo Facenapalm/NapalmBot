d67235fb297b191a34c1761cf550953af7c98128
Facenapalm <Antik125@ya.ru>
2017-04-17 02:06:07 +0300

    Optimizations, extra lists.

--- sort_unrev_files.py
+++ sort_unrev_files.py
@@ -9,6 +9,19 @@ from pywikibot.data.api import Request
 
 DEFAULT_SITE = pywikibot.Site()
 
+TALK_NS = [
+    "Обсуждение",
+    "Обсуждение участника",
+    "Обсуждение Википедии",
+    "Обсуждение файла",
+    "Обсуждение шаблона",
+    "Обсуждение категории",
+    "Обсуждение портала",
+    "Обсуждение Инкубатора",
+    "Обсуждение проекта",
+    "Обсуждение арбитража"
+]
+
 def get_pages_categories(pagelist, site=DEFAULT_SITE, limit=500):
     """
     For every page from the list get list of categories and return
@@ -102,17 +115,18 @@ def collect_info(site=DEFAULT_SITE):
     pagelist = list(set(pagelist))
     catdict = get_pages_categories(pagelist, site=site)
 
-    for key, value in enumerate(result):
+    for value in result:
         if "." in value["filename"]:
             value["extension"] = re.match(r".*\.(.+)$", value["filename"]).group(1).lower()
         else:
             value["extension"] = ""
-        value["filecats"] = catdict[value["filename"]]
 
         categories = []
         for page in value["pages"]:
             categories += catdict[page]
-        value["categories"] = list(set(categories))
+        value["categories"] = set(categories)
+        value["filecats"] = set(catdict[value["filename"]])
+        value["pages"] = set(value["pages"])
 
     return result
 
@@ -120,18 +134,25 @@ def sort_info(info, metapage, site=DEFAULT_SITE):
     """Sort files and create subpages of metapage with corresponding information."""
 
     metaline = "* [[{metapage}/{{page}}|{{page}}]] ({{num}} страниц)".format(metapage=metapage)
-    emptylist = "Всё отпатрулировано. Отличная работа!"
+    emptylist = "<noinclude>Всё отпатрулировано. Отличная работа!</noinclude>"
     listcat = "\n<noinclude>[[Категория:Википедия:Списки неотпатрулированных файлов|{page}]]</noinclude>"
     comment = "Обновление списка."
 
-    def _select_from_cat(pagename, category):
+    def _select_from_cats(pagename, catlist, mode="any"):
         """
-        Select all unreviewed files used on pages from current category.
+        Select all unreviewed files used on pages from current categories.
+        There are 3 modes: "all", "any" and "none".
         Return the line for metapage.
         """
         files = []
         for value in info:
-            if category in value["categories"]:
+            count = 0
+            for category in catlist:
+                if category in value["categories"]:
+                    count += 1
+            if (mode == "any" and count > 0 or
+                mode == "none" and count == 0 or
+                mode == "all" and count == len(catlist)):
                 files.append(value["filename"])
         files = sorted(files)
 
@@ -146,10 +167,19 @@ def sort_info(info, metapage, site=DEFAULT_SITE):
 
         return metaline.format(page=pagename, num=len(files))
 
+    # def _select_from_metacat(pagename, metacat):
+    #     """
+    #     Select all unreviewed files used on pages from this category, its subcategories, etc.
+    #     Return the line for metapage.
+    #     """
+    #     category = pywikibot.Category(site, metacat)
+    #     catlist = [subcat.title() for subcat in category.subcategories(recurse=True)]
+    #     return _select_from_cats(pagename, catlist)
+
     def _select_from_ext(pagename, extensions, reverse=False):
         """
         Select all unreviewed files with given extensions.
-        Return the number of found files.
+        Return the line for metapage.
         """
         files = []
         for value in info:
@@ -171,7 +201,7 @@ def sort_info(info, metapage, site=DEFAULT_SITE):
     def _select_from_use_count(pagename, min_usage=0, max_usage=float("inf")):
         """
         Select all high used unreviewed files.
-        Return the number of found files.
+        Return the line for metapage.
         """
         files = []
         for value in info:
@@ -191,52 +221,93 @@ def sort_info(info, metapage, site=DEFAULT_SITE):
 
         return metaline.format(page=pagename, num=len(files))
 
+    def _select_from_namespaces(pagename, nslist):
+        """
+        Select all unreviewed files which are used on pages with given namespaces.
+        Return the line for metapage.
+        """
+        files = []
+        for value in info:
+            if any([page.startswith(namespace + ":") for namespace in nslist for page in value["pages"]]):
+                files.append(value["filename"])
+        files = sorted(files)
+
+        page = pywikibot.Page(site, metapage + "/" + pagename)
+        if files == []:
+            text = emptylist
+        else:
+            text = "\n".join(["# [[:{}]]".format(name) for name in files])
+            text += listcat.format(page=nslist[0])
+        page.text = text
+        page.save(comment, minor=False)
+
+        return metaline.format(page=pagename, num=len(files))
+
     lines = []
 
-    lines.append("Неотпатрулированные файлы по тематике:")
-    lines.append(_select_from_cat("персоналии", "Категория:Персоналии по алфавиту"))
-    lines.append(_select_from_cat("фильмы", "Категория:Фильмы по алфавиту"))
-    lines.append(_select_from_cat("населённые пункты", "Категория:Населённые пункты по алфавиту"))
-    lines.append(_select_from_cat("альбомы", "Категория:Альбомы по алфавиту"))
-    lines.append(_select_from_cat("компании", "Категория:Компании по алфавиту"))
-    lines.append(_select_from_cat("футбольные клубы", "Категория:Футбольные клубы по алфавиту"))
-    lines.append(_select_from_cat("компьютерные игры", "Категория:Компьютерные игры по алфавиту"))
-    lines.append(_select_from_cat("песни", "Категория:Песни по алфавиту"))
-    lines.append(_select_from_cat("мультфильмы", "Категория:Мультфильмы по алфавиту"))
-    lines.append(_select_from_cat("музыкальные коллективы", "Категория:Музыкальные коллективы по алфавиту"))
-    lines.append(_select_from_cat("программное обеспечение", "Категория:Программное обеспечение по алфавиту"))
-    lines.append(_select_from_cat("водные объекты", "Категория:Водные объекты по алфавиту"))
-    lines.append(_select_from_cat("улицы", "Категория:Улицы по алфавиту"))
-    lines.append(_select_from_cat("телесериалы", "Категория:Телесериалы по алфавиту"))
-    lines.append(_select_from_cat("предприятия", "Категория:Предприятия по алфавиту"))
-    lines.append(_select_from_cat("университеты", "Категория:Университеты по алфавиту"))
-    lines.append(_select_from_cat("литературные произведения", "Категория:Литературные произведения по алфавиту"))
-    lines.append(_select_from_cat("книги", "Категория:Книги по алфавиту"))
-    lines.append(_select_from_cat("станции метрополитена", "Категория:Станции метрополитена по алфавиту"))
-    lines.append(_select_from_cat("культовые сооружения", "Категория:Культовые сооружения по алфавиту"))
-    lines.append(_select_from_cat("хоккейные клубы", "Категория:Хоккейные клубы по алфавиту"))
-    lines.append(_select_from_cat("награды", "Категория:Награды по алфавиту"))
-    lines.append(_select_from_cat("музеи", "Категория:Музеи по алфавиту"))
-    lines.append(_select_from_cat("организации", "Категория:Организации по алфавиту"))
-    lines.append(_select_from_cat("суда", "Категория:Суда по алфавиту"))
-    lines.append(_select_from_cat("оружие", "Категория:Оружие по алфавиту"))
-    lines.append(_select_from_cat("сражения", "Категория:Сражения по алфавиту"))
-    lines.append(_select_from_cat("сайты", "Категория:Сайты по алфавиту"))
-    lines.append(_select_from_cat("телеканалы", "Категория:Телеканалы по алфавиту"))
-    lines.append(_select_from_cat("политические партии", "Категория:Политические партии по алфавиту"))
+    lines.append("Списки неотпатрулированных файлов…")
+    lines.append("")
 
-    lines.append("Используемые в статусных статьях:")
-    lines.append(_select_from_cat("статьи года", "Категория:Википедия:Статьи года по алфавиту"))
-    lines.append(_select_from_cat("избранные статьи", "Категория:Википедия:Избранные статьи по алфавиту"))
-    lines.append(_select_from_cat("хорошие статьи", "Категория:Википедия:Хорошие статьи по алфавиту"))
-    lines.append(_select_from_cat("добротные статьи", "Категория:Википедия:Добротные статьи по алфавиту"))
-    lines.append(_select_from_cat("избранные списки", "Категория:Википедия:Избранные списки по алфавиту"))
+    lines.append("…по тематике:")
+    lines.append(_select_from_cats("персоналии", ["Категория:Персоналии по алфавиту"]))
+    lines.append(_select_from_cats("фильмы", ["Категория:Фильмы по алфавиту"]))
+    lines.append(_select_from_cats("населённые пункты", ["Категория:Населённые пункты по алфавиту"]))
+    lines.append(_select_from_cats("альбомы", ["Категория:Альбомы по алфавиту"]))
+    lines.append(_select_from_cats("компании", ["Категория:Компании по алфавиту"]))
+    lines.append(_select_from_cats("футбольные клубы", ["Категория:Футбольные клубы по алфавиту"]))
+    lines.append(_select_from_cats("компьютерные игры", ["Категория:Компьютерные игры по алфавиту"]))
+    lines.append(_select_from_cats("песни", ["Категория:Песни по алфавиту"]))
+    lines.append(_select_from_cats("мультфильмы", ["Категория:Мультфильмы по алфавиту"]))
+    lines.append(_select_from_cats("музыкальные коллективы", ["Категория:Музыкальные коллективы по алфавиту"]))
+    lines.append(_select_from_cats("программное обеспечение", ["Категория:Программное обеспечение по алфавиту"]))
+    lines.append(_select_from_cats("водные объекты", ["Категория:Водные объекты по алфавиту"]))
+    lines.append(_select_from_cats("улицы", ["Категория:Улицы по алфавиту"]))
+    lines.append(_select_from_cats("телесериалы", ["Категория:Телесериалы по алфавиту"]))
+    lines.append(_select_from_cats("предприятия", ["Категория:Предприятия по алфавиту"]))
+    lines.append(_select_from_cats("университеты", ["Категория:Университеты по алфавиту"]))
+    lines.append(_select_from_cats("литературные произведения", ["Категория:Литературные произведения по алфавиту"]))
+    lines.append(_select_from_cats("книги", ["Категория:Книги по алфавиту"]))
+    lines.append(_select_from_cats("станции метрополитена", ["Категория:Станции метрополитена по алфавиту"]))
+    lines.append(_select_from_cats("культовые сооружения", ["Категория:Культовые сооружения по алфавиту"]))
+    lines.append(_select_from_cats("хоккейные клубы", ["Категория:Хоккейные клубы по алфавиту"]))
+    lines.append(_select_from_cats("награды", ["Категория:Награды по алфавиту"]))
+    lines.append(_select_from_cats("музеи", ["Категория:Музеи по алфавиту"]))
+    lines.append(_select_from_cats("организации", ["Категория:Организации по алфавиту"]))
+    lines.append(_select_from_cats("суда", ["Категория:Суда по алфавиту"]))
+    lines.append(_select_from_cats("оружие", ["Категория:Оружие по алфавиту"]))
+    lines.append(_select_from_cats("сражения", ["Категория:Сражения по алфавиту"]))
+    lines.append(_select_from_cats("сайты", ["Категория:Сайты по алфавиту"]))
+    lines.append(_select_from_cats("телеканалы", ["Категория:Телеканалы по алфавиту"]))
+    lines.append(_select_from_cats("политические партии", ["Категория:Политические партии по алфавиту"]))
 
-    lines.append("Прочее:")
+    lines.append("…по пространствам имён:")
+    lines.append(_select_from_namespaces("из шаблонов", ["Шаблон"]))
+    lines.append(_select_from_namespaces("из порталов", ["Портал"]))
+    lines.append(_select_from_namespaces("из проектов", ["Проект"]))
+    lines.append(_select_from_namespaces("из ПИ Википедия", ["Википедия"]))
+    lines.append(_select_from_namespaces("со страниц участников", ["Участник"]))
+    lines.append(_select_from_namespaces("из обсуждений", TALK_NS))
+
+    lines.append("…по расширению:")
+    lines.append(_select_from_ext("jpg-изображения", ["jpg", "jpeg"]))
+    lines.append(_select_from_ext("png-изображения", ["png"]))
+    lines.append(_select_from_ext("gif-изображения", ["gif"]))
+    lines.append(_select_from_ext("svg-изображения", ["svg"]))
+    lines.append(_select_from_ext("tiff-изображения", ["tif", "tiff"]))
     lines.append(_select_from_ext("не изображения", ["jpg", "jpeg", "png", "gif", "svg", "tif", "tiff"], reverse=True))
+
+    lines.append("…по интенсивности использования:")
     lines.append(_select_from_use_count("самые используемые", min_usage=5))
     lines.append(_select_from_use_count("неиспользуемые", max_usage=0))
 
+    lines.append("…используемых в статусных статьях:")
+    lines.append(_select_from_cats("статьи года", ["Категория:Википедия:Статьи года по алфавиту"]))
+    lines.append(_select_from_cats("избранные статьи", ["Категория:Википедия:Избранные статьи по алфавиту"]))
+    lines.append(_select_from_cats("хорошие статьи", ["Категория:Википедия:Хорошие статьи по алфавиту"]))
+    lines.append(_select_from_cats("добротные статьи", ["Категория:Википедия:Добротные статьи по алфавиту"]))
+    lines.append(_select_from_cats("избранные списки", ["Категория:Википедия:Избранные списки по алфавиту"]))
+
+    lines.append("Если вы не нашли в списке интересующую вас тематику — [[ОУ:Facenapalm|напишите ботоводу]].")
     lines.append("")
     lines.append("[[Категория:Википедия:Списки неотпатрулированных файлов| ]]")
 
