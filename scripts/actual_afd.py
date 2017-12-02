112c2c60e2dc8a8def86a0691ea39259a3b61fdc
Anton Facenapalm <Antik125@ya.ru>
2017-12-03 00:21:08 +0300

    Add actual_afd.py

---
+++ scripts/actual_afd.py
@@ -0,0 +1,17 @@
+"""Update ruwiki's [[ВП:КУП]] shortcut to the actual 'Articles for deletion' page."""
+
+import pywikibot
+
+TEXT = """#REDIRECT [[Википедия:К удалению/{{subst:#time:j xg Y}}]]
+
+[[Категория:Википедия:Механизмы, обрабатываемые NapalmBot]]"""
+
+def main():
+    """Main script function."""
+    site = pywikibot.Site()
+    page = pywikibot.Page(site, "Википедия:КУП")
+    page.text = TEXT
+    page.save("Обновление даты.")
+
+if __name__ == "__main__":
+    main()
