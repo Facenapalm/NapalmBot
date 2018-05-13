6bae2879f029cf8b410377ffec6f44f7b32277a4
Anton Facenapalm <Anton Facenapalm>
2018-05-14 02:29:34 +0300

    New search format

--- scripts/userscripts.py
+++ scripts/userscripts.py
@@ -57,7 +57,7 @@ def main():
     result = "Последнее обновление: {{subst:#time:j xg Y, H:i}}\n\n"
     result += "{| class=\"wikitable sortable\"\n"
     result += "! Место !! Скрипт !! Использований\n"
-    formatstr = "|-\n| {num} || [[Участник:{page}]] || [https://ru.wikipedia.org/w/index.php?search=insource%3A%22{page}%22+prefix%3Au%3A {count}]\n"
+    formatstr = "|-\n| {num} || [[Участник:{page}]] || [https://ru.wikipedia.org/w/index.php?search=user%3A+insource%3A%22{page}%22 {count}]\n"
     for num, page in enumerate(sorted(sorted(stats), key=stats.get, reverse=True)):
         count = stats[page]
         result += formatstr.format(num=num + 1, page=page, count=count)
