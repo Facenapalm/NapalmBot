0aadd4d006d95a97217239900e90cfd213a9a082
Anton Facenapalm <Anton Facenapalm>
2018-06-01 03:24:06 +0300

    10th marker

---
+++ scripts/markers/mark_error_10.py
@@ -0,0 +1,22 @@
+"""Marks all fixed errors #10 on ruwiki's CheckWikipedia."""
+import re
+import pywikibot
+from checkwiki import load_page_list, mark_error_done, log
+
+NUMBER = "10"
+REGEXP = r"\[\[\[|\[\[(?:\]?[^\]\n])*\]?(?:\[\[|$)"
+FLAGS = re.M
+
+def main():
+    """Main script function."""
+    site = pywikibot.Site()
+    for line in load_page_list(NUMBER):
+        page = pywikibot.Page(site, line)
+        if re.search(REGEXP, page.text, flags=FLAGS) is None:
+            mark_error_done(NUMBER, page.title())
+            log(line, success=True)
+        else:
+            log(line, success=False)
+
+if __name__ == "__main__":
+    main()
