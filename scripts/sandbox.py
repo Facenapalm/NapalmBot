43fd240d0da62e804031c11759c5fd39767e958d
Anton Facenapalm <Antik125@ya.ru>
2018-07-06 23:16:00 +0300

    Header restorer for sandbox

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -2,36 +2,38 @@
 Sandbox cleaner for Russian Wikipedia.
 
 Usage:
-    python sandbox.py
+    python sandbox.py [--noclear]
+
+If --noclear is specified, script will restore deleted headers, but will not
+clear sandboxes.
 """
 
+import sys
 import pywikibot
 
 SANDBOXES = [
-    ("Википедия:Песочница", "{{тестируйте ниже}}\n"),
-    ("Обсуждение Википедии:Песочница", "{{тестируйте ниже}}\n"),
-    ("Инкубатор:Песочница", "{{тестируйте ниже}}\n"),
-    ("Шаблон:Песочница", "<noinclude>{{тестируйте ниже}}</noinclude>"),
-    ("Шаблон:Песочница/doc", "<noinclude>{{тестируйте ниже}}</noinclude>\n"),
+    ("Википедия:Песочница", "{{тестируйте ниже}}\n", 15),
+    ("Обсуждение Википедии:Песочница", "{{тестируйте ниже}}\n", 15),
+    ("Инкубатор:Песочница", "{{тестируйте ниже}}\n", 15),
+    ("Шаблон:Песочница", "<noinclude>{{тестируйте ниже}}</noinclude>", 30),
+    ("Шаблон:Песочница/doc", "<noinclude>{{тестируйте ниже}}</noinclude>\n", 30),
 ]
-
-def delayed_edit(page, text, delay=15):
-    """
-    Replace text of the page (first parameter) with given one (second parameter)
-    only if last edit was made more than 15 (can be changed via third parameter)
-    minutes ago.
-    """
-    delta = pywikibot.Timestamp.utcnow() - page.editTime()
-    if delta.total_seconds() >= 60 * delay:
-        page.text = text
-        page.save("Бот: очистка песочницы", force=True)
 
 def main():
     """Main script function."""
     site = pywikibot.Site()
-    for (title, text) in SANDBOXES:
+    time = pywikibot.Timestamp.utcnow()
+    for (title, text, delay) in SANDBOXES:
         page = pywikibot.Page(site, title)
-        delayed_edit(page, text)
+        delta = time - page.editTime()
+        if "--noclear" not in sys.argv and delta.total_seconds() >= 60 * delay:
+            # clear sandbox
+            page.text = text
+            page.save("Бот: очистка песочницы", force=True)
+        elif not page.text.startswith(text.strip()):
+            # restore header
+            page.text = text + page.text
+            page.save("Бот: не удаляйте шаблон, тестируйте ниже!", force=True)
 
 if __name__ == "__main__":
     main()
