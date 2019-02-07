d1b599fe4b2d14988a4071be0aba80ce63a14a0e
Anton Facenapalm <Antik125@ya.ru>
2019-02-08 01:56:10 +0300

    Various fixes

--- scripts/revredirs.py
+++ scripts/revredirs.py
@@ -3,12 +3,25 @@ import re
 import pywikibot
 from pywikibot.data.api import Request
 
-def get_list(site, namespace="0"):
-    """Get list of unreviewed redirects with their targets."""
+def get_review_token(site):
+    """Get a review token on the given site."""
+    return site.get_tokens(["csrf"])["csrf"]
+
+def review(site, token, page):
+    """Review the latest revision of the given page."""
+    revid = page.latest_revision_id
+    request = Request(site=site,
+                      action="review",
+                      token=token,
+                      revid=revid)
+    request.submit()
+
+def get_unreviewed_redirects(site, namespace="0"):
+    """Get a list of the unreviewed redirects with their targets."""
     result = []
 
     def _submit_and_parse(request):
-        """Divide answer to list of values and continue info."""
+        """Divide the answer to the list of values and continue info."""
         answer = request.submit()
         if "pages" not in answer["query"]:
             return ([], {})
@@ -58,66 +71,41 @@ def get_list(site, namespace="0"):
     result = [(x["title"], x["links"][0]) for x in filter(lambda x: len(x["links"]) == 1, result)]
     return result
 
-def compare(redirect, target):
-    """Return true if redirect == target except е → ё, - → — and other transitions."""
-    if len(redirect) != len(target):
-        return False
-
-    # «Елочная ёлка» → «Ёлочная ёлка» doesn't seems to be a legal redirect
-    have_yo = "ё" in redirect
-    have_apo = "’" in redirect
-    have_quote = "«" in redirect or "»" in redirect
-    have_dash = "—" in redirect
-
-    for idx, rchar in enumerate(redirect):
-        tchar = target[idx]
-        if rchar == tchar:
-            continue
-        if rchar == "е" and tchar == "ё":
-            if have_yo:
-                return False
-            else:
-                continue
-        if rchar == "'" and tchar == "’":
-            if have_apo:
-                return False
-            else:
-                continue
-        if (rchar == "\"" and tchar == "«") or (rchar == "\"" and tchar == "»"):
-            if have_quote:
-                return False
-            else:
-                continue
-        if rchar == "-" and tchar == "—":
-            if have_dash:
-                return False
-            else:
-                continue
-        return False
-    return True
-
 def primary_check(redirect, target):
-    """Checks if redirect seeems to be legal."""
+    """Checks if redirect→target pair seeems to be legal."""
+
+    def compare(redirect, target):
+        """Return true if redirect == target except 'е'' → 'ё', '-' → '—' and other replaces."""
+        replaces = {
+            "е": "ё",
+            "'": "’",
+            "\"": "«»„“",
+            "-": "—"
+        }
+        if len(redirect) != len(target):
+            return False
+        for idx, rchar in enumerate(redirect):
+            tchar = target[idx]
+            if rchar == tchar:
+                continue
+            if rchar in replaces:
+                if tchar in replaces[rchar]:
+                    continue
+            return False
+        return True
+
+    # redirects that help people typing page titles
     if compare(redirect, target):
         return True
-    # redirects from one precision to another
-    parser = re.compile(r"^([^\(\)]+) \([^\(\)]+\)$")
-    rmatch = parser.match(redirect)
-    tmatch = parser.match(target)
-    if rmatch and tmatch:
-        # something (one) → something (two)
-        if rmatch.group(1) == tmatch.group(1):
+
+    # redirects to pages with disambiguations
+    match = re.match(r"^([^\(\)]+) \([^\(\)]+\)$", target)
+    if match:
+        if redirect == match.group(1):
             return True
-    elif rmatch:
-        # something (one) → something
-        if rmatch.group(1) == target:
-            return True
-    elif tmatch:
-        # something → something (two)
-        if redirect == tmatch.group(1):
-            return True
+
     # persones redirect
-    match = re.match(r"([а-яё]+), ([а-яё]+) ([а-яё]+)", target, flags=re.I)
+    match = re.match(r"^([а-яё]+), ([а-яё]+) ([а-яё]{4,})$", target, flags=re.I)
     if match:
         candidates = [
             "{surname} {name} {fathername}",
@@ -125,22 +113,24 @@ def primary_check(redirect, target):
             "{surname}, {name}",
             "{surname} {name}",
             "{name} {surname}",
-            "{name}"
+            "{surname}"
         ]
         for candidate in candidates:
             if compare(redirect, candidate.format(surname=match.group(1), name=match.group(2),
                                                   fathername=match.group(3))):
                 return True
-    match = re.match(r"([а-яё]+), ([а-яё]+)", target, flags=re.I)
+
+    match = re.match(r"^([а-яё]+), ([а-яё]+)$", target, flags=re.I)
     if match:
         candidates = [
             "{surname} {name}",
             "{name} {surname}",
-            "{name}"
+            "{surname}"
         ]
         for candidate in candidates:
             if compare(redirect, candidate.format(surname=match.group(1), name=match.group(2))):
                 return True
+
     return False
 
 def secondary_check(rpage, tpage):
@@ -160,35 +150,20 @@ def secondary_check(rpage, tpage):
         return False
     return True
 
-def get_review_token(site):
-    """Get rveiew token on given site."""
-    return site.get_tokens(["csrf"])["csrf"]
-
-def review(site, token, page):
-    """Review given page."""
-    revid = page.latest_revision_id
-    request = Request(site=site,
-                      action="review",
-                      token=token,
-                      revid=revid)
-    request.submit()
-
-def review_list(lst):
-    """Do secondary check and review all redirects from list."""
+def main():
+    """Main script function."""
     site = pywikibot.Site()
+    lst = filter(lambda x: primary_check(x[0], x[1]), get_unreviewed_redirects(site))
+    pywikibot.output("List loaded")
     token = get_review_token(site)
     for redirect, target in lst:
         rpage = pywikibot.Page(site, redirect)
         tpage = pywikibot.Page(site, target)
         if not secondary_check(rpage, tpage):
+            pywikibot.output("[[{}]] → [[{}]]: secondary check failed".format(redirect, target))
             continue
         review(site, token, rpage)
-
-def main():
-    """Main script function."""
-    site = pywikibot.Site()
-    lst = filter(lambda x: primary_check(x[0], x[1]), get_list(site))
-    review_list(lst)
+        pywikibot.output("[[{}]] → [[{}]]: reviewed".format(redirect, target))
 
 if __name__ == "__main__":
     main()
